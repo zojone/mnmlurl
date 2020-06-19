9217f348fba9bf5f6e189e65764f6c9a20324363
zozone <pp00350@gmail.com>
2020-06-19 11:24:25 +1000

    Update head.js

--- src/head.js
+++ src/head.js
@@ -1,4 +1,4 @@
-const endpoint = 'https://www.jsonstore.io/6b6e3cd07f3aaecd8df25ab95875eb2462f00a7614f7205c2d5dc5229896ea98'
+const endpoint = 'https://jsonbox.io/box_dcb0deec984b844a29ec'
 const ebox = document.getElementById('erbox')
 const fetchJSON = (a) => {
 	const f = new XMLHttpRequest()
